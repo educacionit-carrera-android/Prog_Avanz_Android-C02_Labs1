34efc4078a81d64e7c813c5b38f3f0c701f50e2f
carrera.android <carrera.android@educacionit.com>
2020-01-18 13:45:41 -0300

    Labs 1.2

--- app/src/main/java/com/example/fragmentexample/FirstFragment.kt
+++ app/src/main/java/com/example/fragmentexample/FirstFragment.kt
@@ -9,11 +9,26 @@ import android.widget.Button
 
 class FirstFragment : Fragment() {
 
+    private var listener: FirstFragmentListener? = null
+    private lateinit var btnShowContent: Button
+
+    fun setListener(listener: FirstFragmentListener) {
+        this.listener = listener
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         val view = inflater.inflate(R.layout.fragment_first, container, false)
+
+        initializeButton(view)
 
         return view
     }
 
+    private fun initializeButton(view: View) {
+        btnShowContent = view.findViewById(R.id.btnShowContent)
+        btnShowContent.setOnClickListener {
+            listener?.onShowContentClick()
+        }
+    }
 
 }
--- app/src/main/java/com/example/fragmentexample/MainActivity.kt
+++ app/src/main/java/com/example/fragmentexample/MainActivity.kt
@@ -3,20 +3,29 @@ package com.example.fragmentexample
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), FirstFragmentListener {
+
+    private lateinit var firstFragment: FirstFragment
     private var secondFragment: SecondFragment = SecondFragment()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        addSecondFragment()
+        firstFragment = supportFragmentManager.findFragmentById(R.id.firstFragmentContainer) as FirstFragment
+        firstFragment.setListener(this)
     }
 
     private fun addSecondFragment() {
-        supportFragmentManager
-                .beginTransaction()
-                .add(R.id.secondFragmentContainer, secondFragment, "SecondFragment")
-                .commit()
+        if (supportFragmentManager.findFragmentByTag("SecondFragment") == null) {
+            supportFragmentManager
+                    .beginTransaction()
+                    .add(R.id.secondFragmentContainer, secondFragment, "SecondFragment")
+                    .commit()
+        }
+    }
+
+    override fun onShowContentClick() {
+        addSecondFragment()
     }
 }
