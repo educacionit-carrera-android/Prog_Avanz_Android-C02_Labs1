c6fd46c7e6241a5aa4cbc025515b7c99584d2372
carrera.android <carrera.android@educacionit.com>
2020-01-27 01:50:23 -0300

    Migrate to Android X

--- app/src/main/java/com/example/fragmentexample/FirstFragment.kt
+++ app/src/main/java/com/example/fragmentexample/FirstFragment.kt
@@ -1,13 +1,13 @@
 package com.example.fragmentexample
 
 import android.os.Bundle
-import android.support.v4.app.Fragment
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 
-class FirstFragment : Fragment() {
+class FirstFragment : androidx.fragment.app.Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         val view = inflater.inflate(R.layout.fragment_first, container, false)
